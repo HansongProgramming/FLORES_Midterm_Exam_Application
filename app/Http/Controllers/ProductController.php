840bcbbd79b86709a9ef216e4c7a0e991acd3c6f
Hansongli <hlf1850@students.uc-bcf.edu.ph>
2025-02-24 21:00:10 +0800

    Added ProductController with products array

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $movies = [
+            ['title' => 'The Gorge', 'duration' => 120, 'category' => 'scifi'],
+            ['title' => 'Interstellar', 'duration' => 145, 'category' => 'scifi'],
+            ['title' => 'Everything Everywhere All At Once', 'duration' => 160, 'category' => 'drama'],
+            ['title' => 'Train to Busan', 'duration' => 107, 'category' => 'horror']
+        ];
+
+        return view('movies', compact('products'));
+    }
+}
